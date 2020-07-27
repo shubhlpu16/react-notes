4428fd6e11ef1bf7d907f86cccaecc7ebd559ef3
Shubham Saxena <shubh.lpu16@gmail.com>
2020-07-27 19:15:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import Home from './routes/home/Home';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+    </Switch>
+
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ContextProvider} from './AppContext';
+import store from './store/index';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider value={{store}}>
+    <Router>
+      <App/>
+    </Router>
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
